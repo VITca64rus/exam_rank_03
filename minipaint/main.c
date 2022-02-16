0629a676f2ac071e7adafb12f64d8f2d745ae5d9
Sarsfield Zelda <sazelda@am-k3.msk.21-school.ru>
2022-02-16 17:44:06 +0300

    first commit

---
+++ minipaint/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_exit(FILE *file, char *map)
+{
+	if (file)
+		fclose(file);
+	if (map)
+		free(map);
+	write(1, "Error: Operation file corrupted\n", 32);
+	return(1);
+}
+
+int	main(int argc, char **argv)
+{
+	FILE	*file = NULL;
+	char	*map = NULL;
+
+	if (argc != 2)
+	{
+		write(1, "Error: argument\n", 16);
+		return(1);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+		return(ft_exit(file, map));
+	return(0);
+}
